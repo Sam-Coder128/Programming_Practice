ec70e45c485c47a2d971f09eb7be8636df816bd5
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-04-27 21:59:34 +0530

    Create DataType.java

---
+++ Java/DataType.java
@@ -0,0 +1,15 @@
+public class DataType {
+    public static void main(String[] args) {
+        int age = 20;
+        double pi = 3.14;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Samruddh";
+
+        System.out.println("Age: " + age);
+        System.out.println("Pi: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Java Fun? " + isJavaFun);
+        System.out.println("Name: " + name);
+    }
+}
