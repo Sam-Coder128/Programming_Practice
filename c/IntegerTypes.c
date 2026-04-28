869e9c8e4a3d027ee7c934fb732d6ac0b88a5ebf
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-04-28 23:03:30 +0530

    Create IntegerTypes.c

---
+++ c/IntegerTypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    short int a = -10;
+    long int b = 100000;
+    unsigned int c = 50;
+    signed int d = -20;
+
+    printf("Short int: %d\n", a);
+    printf("Long int: %ld\n", b);
+    printf("Unsigned int: %u\n", c);
+    printf("Signed int: %d\n", d);
+
+    return 0;
+}
