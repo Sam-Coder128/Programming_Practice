3af313bffd20266b448eb6fb2c8f0aa2a12ffe5b
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-02 14:48:07 +0530

    Create Access.java

---
+++ Java/Access.java
@@ -0,0 +1,26 @@
+class Demo{
+    public int i;
+    private int j;
+    protected int k;
+    public Demo(){
+        i=0;
+        j=0;
+        k=0;
+    }
+    public void Display(){
+        System.out.println("Value of i: "+i); // Allowed
+        System.out.println("Value of j: "+j); // Allowed
+        System.out.println("Value of k: "+k); // Allowed
+    }
+}
+
+public class Access{
+    public static void main(String A[]) {
+        Demo dobj = new Demo();
+        dobj.Display();
+    }
+
+    System.out.println("Value of i: "+dobj.i); //Allowed
+    System.out.println("Value of j: "+dobj.j); // Not Allowed in main becoz its private
+    System.out.println("Value of k: "+dobj.k); // Allowed in inheritance not main
+}
