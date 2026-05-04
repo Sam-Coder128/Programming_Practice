430bb5b3a5c39d64d90d5662759723f7a37b9c6c
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-04 05:51:31 +0530

    Create ThisSuperX.java

---
+++ Java/ThisSuperX.java
@@ -0,0 +1,32 @@
+class Base{
+    public int i;
+    public int j;
+    public Base(){
+        this.i=11;
+        this.j=21;
+        System.out.println("Inside Base constructor.");
+    }
+    public void Fun(){
+        System.out.println("Inside Fun Base.");
+    }
+}
+class Derived extends Base{
+    public int x;
+    public Derived(){
+        this.x=51;
+        System.out.println("Inside Derived constructor.");
+    }
+    public void Gun(){
+        System.out.println("Inside Gun Derived.");
+        System.out.println("Value of i: "+i);
+        System.out.println("Value of j: "+j);
+        System.out.println("Value of x: "+x);
+    }
+}
+
+class ThisSuperX{
+public static void main(String[] args) {
+        Derived Dobj = new Derived();
+        Dobj.Gun();
+    }
+}
