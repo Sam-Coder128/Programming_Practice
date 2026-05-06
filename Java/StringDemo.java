b411f9dd4c7605644aab4a379363a8fad83e4b09
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-06 07:15:00 +0530

    Create StringDemo.java

---
+++ Java/StringDemo.java
@@ -0,0 +1,13 @@
+class StringDemo{
+    public static void main(String[] args) {
+        
+        String S1 = "Hello";
+        String S2 = new String("Ganesh");
+
+        System.out.println(S1);
+        System.out.println(S1);
+
+        System.out.println(S1.length());
+        System.out.println(S2.length());
+    }
+}
