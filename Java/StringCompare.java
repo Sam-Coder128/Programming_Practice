9e82d0ae6dfd15c4079fa581b3b377ae9003586f
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-06 07:16:54 +0530

    Create StringCompare.java

---
+++ Java/StringCompare.java
@@ -0,0 +1,11 @@
+class StringCompare{
+    public static void main(String[] args) {
+
+        String S1 = "Ganesh";
+        String S2 = new String("Ganesh");
+
+        if(S1 == S2)  System.out.println("Strings are equal.");
+        
+        else System.out.println("Strings are unequal.");
+    }
+}
