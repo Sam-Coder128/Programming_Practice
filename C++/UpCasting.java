df9a159c4c921087ed959c5cb4ecdb98eaaac076
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-06 07:18:59 +0530

    Create UpCasting.cpp

---
+++ C++/UpCasting.java
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+public:
+    int i, j;
+};
+
+class Derived: public Base{
+public:
+    int x, y;
+};
+
+int main(){
+    Base *bp = NULL;
+    Derived dobj;
+    bp = &dobj;               // UpCasting
+
+    return 0;
+}
