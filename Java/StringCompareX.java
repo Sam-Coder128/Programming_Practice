f157964ed90bfcaaf65757782062ff9b2afd10c5
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-07 19:43:09 +0530

    Create StringCompareX.java

---
+++ Java/StringCompareX.java
@@ -0,0 +1,11 @@
+class StringCompareX{
+    public static void main(String[] args) {
+
+        String S1 = "Ganesh";
+        String S2 = new String("Ganesh");
+
+        if(S1.equals(S2))  System.out.println("Strings are equal.");
+        
+        else System.out.println("Strings are unequal.");
+    }
+}   
