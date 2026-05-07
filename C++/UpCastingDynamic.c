6177501a51bdebb850ce51cdb62c61d956ee3b55
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-07 19:45:00 +0530

    Create UpCastingDynamic.c

---
+++ C++/UpCastingDynamic.c
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+public:
+    int i, j;
+};
+
+class Derived: public Base{
+public:
+    int x, y;
+};
+
+int main(){
+    Base *bp = NULL;
+    
+    bp = new Derived();               // UpCasting in dynamic
+
+    return 0;
+}
