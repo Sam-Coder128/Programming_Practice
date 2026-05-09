f40b8e71bd8a15980b5e4d91197aadfb4354c3a2
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-09 19:03:56 +0530

    Update StaticData.cpp

--- C++/StaticData.cpp
+++ C++/StaticData.cpp
@@ -1,20 +1,32 @@
 #include<iostream>
 using namespace std;
 
+///////////////////////////////////////////////////////////////
+//
+// Class Name : Demo
+// Members :    int i, int j, static int k
+// Description: Demonstrates static variable excluded from object size.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
 class Demo {
-    public:
-        int i;          // instance variable
-        int j;          // instance variable
-        static int k;   // static variable (class-level)
+public:
+    int i;          // instance variable
+    int j;          // instance variable
+    static int k;   // static variable (class-level)
 };
 
 int Demo::k = 0; // definition of static member outside class
+
+///////////////////////////////////////////////////////////////
+//
+// Application : Shows sizeof() ignores static members.
+//
+///////////////////////////////////////////////////////////////
 
 int main() {
     Demo dobj;
     cout << sizeof(dobj) << endl; // 8
     return 0;
 }
-
-
-// Output is 8 becoz size of object only access local variables not static.
