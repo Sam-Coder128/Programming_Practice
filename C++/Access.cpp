531cf4c29944b5b51ba31ca995696393894f941f
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-09 18:48:51 +0530

    Update Access.cpp

--- C++/Access.cpp
+++ C++/Access.cpp
@@ -1,30 +1,60 @@
 #include<iostream>
 using namespace std;
 
-class Demo{
+///////////////////////////////////////////////////////////////
+//
+// Class Name : Demo
+// Members :    int i (public), int j (private), int k (protected)
+// Description: Demonstrates access specifiers in C++.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+class Demo {
     public:
-        int i;
+        int i;          // Public member
     private:
-        int j;
+        int j;          // Private member
     protected:
-        int k;
+        int k;          // Protected member
+
     public:
-        Demo(){
-            i=0;
-            j=0;
-            k=0;
+        // Constructor
+        Demo() {
+            i = 0;
+            j = 0;
+            k = 0;
         }
-        void display(){
-            cout << "Value of i: " << i << endl; // Allowed
-            cout << "Value of j: " << j << endl; // Allowed
-            cout << "Value of k: " << k << endl; // Allowed
+
+        // Display method to show values of all members
+        void display() {
+            cout << "Value of i: " << i << endl;
+            cout << "Value of j: " << j << endl;
+            cout << "Value of k: " << k << endl;
         }
 };
-int main(){
+
+///////////////////////////////////////////////////////////////
+//
+// Application demonstrating access specifiers
+//
+///////////////////////////////////////////////////////////////
+
+int main() {
     Demo dobj;
-    dobj.display();
-    cout << "Value of i: " << dobj.i << endl; // Allowed
-    cout << "Value of j: " << dobj.j << endl; // Not Allowed
-    cout << "Value of k: " << dobj.k << endl; // Not Allowed
+
+    // Input section
+    cout << "Enter value for i: ";
+    cin >> dobj.i;
+
+    // Output section
+    cout << "Displaying values through class method:" << endl;
+    dobj.display(); // Allowed: method has access to all members
+
+    cout << "Accessing public member directly: " << dobj.i << endl;
+
+    // cout << dobj.j; // ❌ Not Allowed (private)
+    // cout << dobj.k; // ❌ Not Allowed (protected)
+
     return 0;
 }
