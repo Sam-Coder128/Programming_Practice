2bc374a8c78f0e60ecf550ad31c622e82b303dc9
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-09 18:56:22 +0530

    Update Maximum_3.cpp

--- C++/Maximum_3.cpp
+++ C++/Maximum_3.cpp
@@ -1,16 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+///////////////////////////////////////////////////////////////
+//
+// Program Name : Maximum_3
+// Input :        int a, int b, int c (user-entered numbers)
+// Output :       int (largest among the three numbers)
+// Description:   Determines the largest of three integers
+//                using conditional statements.
+// Author:        Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+//
+// Application demonstrating maximum of three numbers
+//
+///////////////////////////////////////////////////////////////
 
 int main() {
     int a, b, c;
+
+    cout << "Enter three numbers: ";
     cin >> a >> b >> c;
 
     if(a >= b && a >= c)
-        cout << "Largest: " << a;
+        cout << "Largest: " << a << endl;
     else if(b >= c)
-        cout << "Largest: " << b;
+        cout << "Largest: " << b << endl;
     else
-        cout << "Largest: " << c;
+        cout << "Largest: " << c << endl;
 
     return 0;
 }
