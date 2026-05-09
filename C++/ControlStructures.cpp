ba89b4e6012e306945ff3e8690c9c98f14992c48
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-09 18:52:03 +0530

    Update ControlStructures.cpp

--- C++/ControlStructures.cpp
+++ C++/ControlStructures.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int num = 10;
+///////////////////////////////////////////////////////////////
+//
+// Program Name : ControlStructures
+// Input :        int (user-entered number)
+// Output :       int
+// Description:   Demonstrates basic control structures in C++
+//                using if-else, switch, and for loop.
+// Author:        Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
 
+int main() {
+    int num = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // if-else demonstration
     if (num > 0)
         cout << "Positive" << endl;
     else
         cout << "Non-positive" << endl;
 
+    // switch-case demonstration
     switch(num) {
-        case 10: cout << "Ten" << endl; break;
-        default: cout << "Other" << endl;
+        case 10: 
+            cout << "Ten" << endl; 
+            break;
+        default: 
+            cout << "Other" << endl;
     }
 
+    // for loop demonstration
+    cout << "Counting from 1 to 5:" << endl;
     for (int i = 1; i <= 5; i++)
         cout << "Count: " << i << endl;
 
