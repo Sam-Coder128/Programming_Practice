0f53e64d363a6aafa4b88e69086cae462c4cb85e
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-09 19:00:04 +0530

    Update SingleLevel.c

--- C++/SingleLevel.c
+++ C++/SingleLevel.c
@@ -1,28 +1,45 @@
 #include<iostream>
 using namespace std;
 
-class Base{
-    public:
-        int i, j;
+///////////////////////////////////////////////////////////////
+//
+// Class Name : Base
+// Members :    int i, int j
+// Methods :    Constructor, Destructor, Fun()
+// Description: Demonstrates a simple base class with
+//              constructor, destructor, and method.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
 
-        Base(){
-            cout << "Inside Base Constructor" << endl;
-            i=0;
-            j=0;
-        }
-        ~Base(){
-            cout << "Inside Base Destructor." << endl;
-        }
-        void Fun(){
-            cout << "Inside Base Fun." << endl;
-        }
-};
-class Derived: public Base{
+class Base {
+public:
+    int i, j;
 
+    Base() {
+        cout << "Inside Base Constructor" << endl;
+        i = 0;
+        j = 0;
+    }
+    ~Base() {
+        cout << "Inside Base Destructor." << endl;
+    }
+    void Fun() {
+        cout << "Inside Base Fun." << endl;
+    }
 };
-int main(){
-    Base bobj;
-    Derived dobj;
+
+class Derived : public Base { };
+
+///////////////////////////////////////////////////////////////
+//
+// Application demonstrating single-level inheritance
+//
+///////////////////////////////////////////////////////////////
+
+int main() {
+    Base bobj;     // Base constructor & destructor run
+    Derived dobj;  // Derived constructor & destructor run (with Base)
 
     return 0;
 }
