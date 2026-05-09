ea776fb525558b5c5523fd260a8748edb3c629de
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-09 18:49:31 +0530

    Update ClassBasics.cpp

--- C++/ClassBasics.cpp
+++ C++/ClassBasics.cpp
@@ -1,20 +1,55 @@
 #include <iostream>
 using namespace std;
 
+///////////////////////////////////////////////////////////////
+//
+// Class Name : Student
+// Members :    int roll, string name
+// Methods :    setData(int, string), display()
+// Description: Demonstrates basics of class and object in C++.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
 class Student {
-    int roll;
-    string name;
+    int roll;          // Roll number
+    string name;       // Student name
 public:
+    // Method to set data
     void setData(int r, string n) {
-        roll = r; name = n;
+        roll = r;
+        name = n;
     }
+
+    // Method to display student details
     void display() {
         cout << "Roll: " << roll << ", Name: " << name << endl;
     }
 };
 
+///////////////////////////////////////////////////////////////
+//
+// Application demonstrating class and object usage
+//
+///////////////////////////////////////////////////////////////
+
 int main() {
     Student s1;
-    s1.setData(1, "Sam");
+
+    // Input section
+    int rno;
+    string nm;
+    cout << "Enter roll number: ";
+    cin >> rno;
+    cout << "Enter name: ";
+    cin >> nm;
+
+    // Setting data
+    s1.setData(rno, nm);
+
+    // Output section
+    cout << "Student details:" << endl;
     s1.display();
+
+    return 0;
 }
