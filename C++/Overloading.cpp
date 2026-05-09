0e2cb2f936bd94362dc27fdedc43b4949d5e07d5
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-09 18:58:50 +0530

    Update Overloading.cpp

--- C++/Overloading.cpp
+++ C++/Overloading.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 using namespace std;
 
+///////////////////////////////////////////////////////////////
+//
+// Class Name : Math
+// Methods :    add(int, int), add(double, double)
+// Input :       int a, int b OR double a, double b
+// Output :      Sum of the two numbers (int or double)
+// Description:  Demonstrates function overloading in C++
+// Author:       Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
 class Math {
 public:
-    int add(int a, int b) { return a+b; }
-    double add(double a, double b) { return a+b; }
+    int add(int a, int b) { return a + b; }
+    double add(double a, double b) { return a + b; }
 };
+
+///////////////////////////////////////////////////////////////
+//
+// Application demonstrating function overloading
+//
+///////////////////////////////////////////////////////////////
 
 int main() {
     Math m;
-    cout << m.add(2,3) << endl;
-    cout << m.add(2.5,3.5) << endl;
+
+    cout << "Addition of integers (2, 3): " << m.add(2, 3) << endl;
+    cout << "Addition of doubles (2.5, 3.5): " << m.add(2.5, 3.5) << endl;
+
+    return 0;
 }
