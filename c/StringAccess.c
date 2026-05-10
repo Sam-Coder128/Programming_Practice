5a23c8d6a80d860de0d020f8f91c7d5fa637eb5b
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:42:04 +0530

    Update StringAccess.c

--- c/StringAccess.c
+++ c/StringAccess.c
@@ -1,8 +1,26 @@
-#include<stdio.h>
-int main(){
-    char Arr[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
-    int i=0;
+///////////////////////////////////////////////////////////////
+//
+// Function Name : main
+// Output        : char values
+// Description   : Shows traversal of character array using loop.
+// Author        : Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
 
-    for ( i = 0; i < 6; i++)
-        printf("%c\n", Arr[i]);   
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+// Application : Explains accessing each character in a string array.
+//
+///////////////////////////////////////////////////////////////
+
+int main() {
+    char Arr[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    int i = 0;
+
+    for (i = 0; i < 6; i++)
+        printf("%c\n", Arr[i]);
+
+    return 0;
 }
