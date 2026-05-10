b5e7e4e3d4780557452055bc5d29fedcbdd15744
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:42:38 +0530

    Update StringAccessX.c

--- c/StringAccessX.c
+++ c/StringAccessX.c
@@ -1,19 +1,33 @@
-#include<stdio.h>
-int main() {
+///////////////////////////////////////////////////////////////
+//
+// Function Name : main
+// Output        : char values
+// Description   : Illustrates pointer traversal of a string.
+// Author        : Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
 
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+// Application : Clarifies how pointer increments move through characters.
+//
+///////////////////////////////////////////////////////////////
+
+int main() {
     char str[] = "Ganesh";
     char* ptr = NULL;
 
-    ptr = str;            //100
+    ptr = str;            // points to first character
 
-    printf("%c\n", *ptr);       //G
-    ptr++;                      //101
+    printf("%c\n", *ptr); // G
+    ptr++;                // move to next character
 
-    printf("%c\n", *ptr);       //a
+    printf("%c\n", *ptr); // a
+    ptr++;                
 
-    ptr++;                      //102
-
-    printf("%c\n", *ptr);       //n
+    printf("%c\n", *ptr); // n
 
     return 0;    
 }
