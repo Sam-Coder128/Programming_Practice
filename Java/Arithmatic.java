3ec8c62821bf943fbf2c644c26689df5250cdda0
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:15:01 +0530

    Update Arithmatic.java

--- Java/Arithmatic.java
+++ Java/Arithmatic.java
@@ -1,32 +1,51 @@
-class Arithmatic{
-    public int No1,No2;
-    public Arithmatic(){
-        this.No1=0;
-        this.No2=0;
+///////////////////////////////////////////////////////////////
+//
+// Class Name : Arithmatic
+// Members :    int No1, int No2
+// Methods :    Constructors, Addition(), Substraction()
+// Description: Highlights constructor overloading with arithmetic operations.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+class Arithmatic {
+    public int No1, No2;
+
+    public Arithmatic() {
+        this.No1 = 0;
+        this.No2 = 0;
     }
-    public Arithmatic(int Value1, int Value2){
-        this.No1=Value1;
-        this.No2=Value2;
+
+    public Arithmatic(int Value1, int Value2) {
+        this.No1 = Value1;
+        this.No2 = Value2;
     }
-    public int Addition(){
-        int Ans=this.No1+this.No2;
-        return  Ans;
+
+    public int Addition() {
+        return this.No1 + this.No2;
     }
-    public int Substraction(){
-        int Ans=this.No1-this.No2;
-        return  Ans;
+
+    public int Substraction() {
+        return this.No1 - this.No2;
     }
 }
-class First{
+
+///////////////////////////////////////////////////////////////
+//
+// Application : Explains constructor usage and arithmetic method calls.
+//
+///////////////////////////////////////////////////////////////
+
+class First {
     public static void main(String A[]) {
-            System.out.println("Inside main");   
+        System.out.println("Inside main");   
 
-            Arithmatic  aobj = new Arithmatic(11,10);
+        Arithmatic aobj = new Arithmatic(11, 10);
 
-            int Result = aobj.Addition();
-            System.out.println("Addition is : " + Result);
+        int Result = aobj.Addition();
+        System.out.println("Addition is : " + Result);
 
-            Result = aobj.Substraction();
-            System.out.println("Subtraction is : " + Result);
-        }
+        Result = aobj.Substraction();
+        System.out.println("Subtraction is : " + Result);
+    }
 }
