3587009f0e5a7f3c4f0853ddf62c10873f6b3fea
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:21:12 +0530

    Update SingleLevel.java

--- Java/SingleLevel.java
+++ Java/SingleLevel.java
@@ -1,42 +1,68 @@
-class Base{
+///////////////////////////////////////////////////////////////
+//
+// Classes : Base, Derived
+// Members : Base → int i, j; Derived → int x
+// Methods : Constructors, Fun(), Gun()
+// Description: Clarifies single-level inheritance with constructor chaining.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+class Base {
     public int i;
     public int j;
-    public Base(){
-        this.i=0;
-        this.j=0;
+
+    public Base() {
+        this.i = 0;
+        this.j = 0;
         System.out.println("Inside Base constructor.");
     }
-    protected void finalise(){
-        System.out.println("Inside Base Finalise.");
+
+    @Override
+    protected void finalize() {
+        System.out.println("Inside Base Finalize.");
     }
-    public void Fun(){
+
+    public void Fun() {
         System.out.println("Inside Fun Base.");
     }
 }
-class Derived extends Base{
+
+class Derived extends Base {
     public int x;
-    public Derived(){
-        this.x=0;
+
+    public Derived() {
+        this.x = 0;
         System.out.println("Inside Derived constructor.");
     }
-    protected void finalise(){
-        System.out.println("Inside Derived Finalise.");
+
+    @Override
+    protected void finalize() {
+        System.out.println("Inside Derived Finalize.");
     }
-    public void Gun(){
+
+    public void Gun() {
         System.out.println("Inside Gun Derived.");
     }
 }
 
-class SingleLevel{
-public static void main(String[] args) {
-    System.out.println("Inside main.");
+///////////////////////////////////////////////////////////////
+//
+// Application : Highlights member accessibility and method calls in inheritance.
+//
+///////////////////////////////////////////////////////////////
 
-    Derived Dobj = new Derived();
-    System.out.println(Dobj.i);
-    System.out.println(Dobj.j);
-    System.out.println(Dobj.x);
-    Dobj.Fun();
-    Dobj.Gun();
-    System.out.println("End of main.");
-}
+class SingleLevel {
+    public static void main(String[] args) {
+        System.out.println("Inside main.");
+
+        Derived Dobj = new Derived();
+        System.out.println(Dobj.i);
+        System.out.println(Dobj.j);
+        System.out.println(Dobj.x);
+        Dobj.Fun();
+        Dobj.Gun();
+
+        System.out.println("End of main.");
+    }
 }
