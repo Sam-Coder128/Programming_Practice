bd0f51072f4e5bd568234c8eec55eebd69c1cd79
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:24:29 +0530

    Update ThisSuper.java

--- Java/ThisSuper.java
+++ Java/ThisSuper.java
@@ -1,31 +1,52 @@
-class Base{
+///////////////////////////////////////////////////////////////
+//
+// Classes : Base, Derived
+// Members : Base → int i, j; Derived → int x
+// Methods : Constructors, Fun(), Gun()
+// Description: Clarifies usage of this and super keywords in inheritance.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+class Base {
     public int i;
     public int j;
-    public Base(){
-        this.i=11;
-        this.j=21;
+
+    public Base() {
+        this.i = 11;
+        this.j = 21;
         System.out.println("Inside Base constructor.");
     }
-    public void Fun(){
+
+    public void Fun() {
         System.out.println("Inside Fun Base.");
     }
 }
-class Derived extends Base{
+
+class Derived extends Base {
     public int x;
-    public Derived(){
-        this.x=51;
+
+    public Derived() {
+        this.x = 51;
         System.out.println("Inside Derived constructor.");
     }
-    public void Gun(){
+
+    public void Gun() {
         System.out.println("Inside Gun Derived.");
-        System.out.println("Value of i: "+super.i);
-        System.out.println("Value of j: "+super.j);
-        System.out.println("Value of x: "+this.x);
+        System.out.println("Value of i: " + super.i);   // accessing parent members
+        System.out.println("Value of j: " + super.j);   // accessing parent members
+        System.out.println("Value of x: " + this.x);    // accessing current class member
     }
 }
 
-class ThisSuper{
-public static void main(String[] args) {
+///////////////////////////////////////////////////////////////
+//
+// Application : Highlights member access using this and super keywords.
+//
+///////////////////////////////////////////////////////////////
+
+class ThisSuper {
+    public static void main(String[] args) {
         Derived Dobj = new Derived();
         Dobj.Gun();
     }
