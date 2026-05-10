84347f2c03185ccd55dde676772cdb911e05732e
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:23:01 +0530

    Update StringCompareX.java

--- Java/StringCompareX.java
+++ Java/StringCompareX.java
@@ -1,11 +1,26 @@
-class StringCompareX{
+///////////////////////////////////////////////////////////////
+//
+// Class Name : StringCompareX
+// Variables : String S1, String S2
+// Description: Explains content comparison using equals() method in Strings.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+class StringCompareX {
+    ///////////////////////////////////////////////////////////////
+    //
+    // Application : Shows equals() usage for string content equality.
+    //
+    ///////////////////////////////////////////////////////////////
     public static void main(String[] args) {
 
-        String S1 = "Ganesh";
-        String S2 = new String("Ganesh");
+        String S1 = "Ganesh";                  // string literal
+        String S2 = new String("Ganesh");      // new object
 
-        if(S1.equals(S2))  System.out.println("Strings are equal.");
-        
-        else System.out.println("Strings are unequal.");
+        if (S1.equals(S2))  
+            System.out.println("Strings are equal.");
+        else 
+            System.out.println("Strings are unequal.");
     }
-}   
+}
