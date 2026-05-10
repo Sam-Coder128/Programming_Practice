ab2e0c5e3cce3a7694db37475c71762d9a6b2e54
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:22:30 +0530

    Update StringCompare.java

--- Java/StringCompare.java
+++ Java/StringCompare.java
@@ -1,11 +1,26 @@
-class StringCompare{
+///////////////////////////////////////////////////////////////
+//
+// Class Name : StringCompare
+// Variables : String S1, String S2
+// Description: Explains difference between == and equals() in string comparison.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+class StringCompare {
+    ///////////////////////////////////////////////////////////////
+    //
+    // Application : Highlights reference vs content comparison in Strings.
+    //
+    ///////////////////////////////////////////////////////////////
     public static void main(String[] args) {
 
-        String S1 = "Ganesh";
-        String S2 = new String("Ganesh");
+        String S1 = "Ganesh";                  // string literal
+        String S2 = new String("Ganesh");      // new object
 
-        if(S1 == S2)  System.out.println("Strings are equal.");
-        
-        else System.out.println("Strings are unequal.");
+        if (S1 == S2)  
+            System.out.println("Strings are equal.");
+        else 
+            System.out.println("Strings are unequal.");
     }
 }
