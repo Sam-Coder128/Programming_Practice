5ebac39304c45a4f75139f10ac34b92d290f39cf
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:39:08 +0530

    Update ReadFile.c

--- c/ReadFile.c
+++ c/ReadFile.c
@@ -1,26 +1,39 @@
-#include<stdio.h>
-#include<fcntl.h>
-#include<unistd.h>
+///////////////////////////////////////////////////////////////
+//
+// Function Name : main
+// Output        : printed values
+// Description   : Shows file opening and reading using open() and read().
+// Author        : Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+///////////////////////////////////////////////////////////////
+//
+// Application : Explains reading bytes from file into buffer and printing.
+//
+///////////////////////////////////////////////////////////////
 
 int main() {
-
     int fd = 0, res = 0;
     char Arr[20];
 
     fd = open("Marvellous.txt", O_RDWR);
 
-    if (fd == -1) printf("Unable to open file.");
-
+    if (fd == -1) 
+        printf("Unable to open file.\n");
     else {
-        printf("File opened with fd: %d.\n",fd);
-        res=read(fd, Arr, 6);
+        printf("File opened with fd: %d.\n", fd);
+
+        res = read(fd, Arr, 6);   // read 6 bytes into Arr
 
         printf("%d bytes read from file.\n", res);
-
-        printf("%s\n", Arr);
+        printf("%s\n", Arr);      // print buffer contents
     }
 
     close(fd);
-
     return 0;
 }
