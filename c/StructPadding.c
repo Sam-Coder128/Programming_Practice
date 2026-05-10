8739f13159ab79f742a67ecf75cde5796b51ded2
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:48:48 +0530

    Update StructPadding.c

--- c/StructPadding.c
+++ c/StructPadding.c
@@ -1,18 +1,33 @@
+///////////////////////////////////////////////////////////////
+//
+// Function Name : main
+// Output        : sizes of struct objects
+// Description   : Clarifies difference between default alignment and pragma pack.
+// Author        : Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 
 // default alignment
 struct DefaultStruct {
-    char a;
-    int b;
-};
+    char a;   // 1 byte + 3 bytes padding
+    int b;    // 4 bytes
+};            // total 8 bytes
 
 // force 1-byte packing
 #pragma pack(1)
 struct PackedStruct {
-    char a;
-    int b;
-};
+    char a;   // 1 byte
+    int b;    // 4 bytes
+};            // total 5 bytes
 #pragma pack() // reset to default
+
+///////////////////////////////////////////////////////////////
+//
+// Application : Demonstrates how packing alters struct size.
+//
+///////////////////////////////////////////////////////////////
 
 int main() {
     printf("Size of DefaultStruct: %lu\n", sizeof(struct DefaultStruct));
