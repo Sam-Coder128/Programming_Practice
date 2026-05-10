aef3dc503a04e4e43ec15cf31ff7789e1ec79e10
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:46:29 +0530

    Update StructArrayX.c

--- c/StructArrayX.c
+++ c/StructArrayX.c
@@ -1,22 +1,34 @@
-#include<stdio.h>
+///////////////////////////////////////////////////////////////
+//
+// Function Name : main
+// Output        : float value, int array values
+// Description   : Explains struct member assignment and printing.
+// Author        : Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
 
-struct Hello
-{
-    float f;        // 4
-    int Arr[3];     // 12
-}hobj;              // 16
+#include <stdio.h>
 
-int main()
-{
+///////////////////////////////////////////////////////////////
+//
+// Application to access and print struct members.
+//
+///////////////////////////////////////////////////////////////
+
+struct Hello {
+    float f;        // 4 bytes
+    int Arr[3];     // 12 bytes
+} hobj;             // total 16 bytes
+
+int main() {
     hobj.f = 10.0f;
 
     hobj.Arr[0] = 11;
     hobj.Arr[1] = 21;
     hobj.Arr[2] = 51;
     
-    printf("%f\n",hobj.f);
-
-    printf("%d\t%d\t%d\n",hobj.Arr[0],hobj.Arr[1],hobj.Arr[2]);
+    printf("%f\n", hobj.f);
+    printf("%d\t%d\t%d\n", hobj.Arr[0], hobj.Arr[1], hobj.Arr[2]);
 
     return 0;
 }
