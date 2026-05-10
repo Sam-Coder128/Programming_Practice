b9b7da80e1f038828b9468d344c20634bdb14897
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:50:58 +0530

    Update StringCreate.c

--- StringCreate.c
+++ StringCreate.c
@@ -1,10 +1,28 @@
-#include<stdio.h>
-int main(){
-    char Arr[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
-    char Brr[] = {'H', 'e', 'l', 'l', 'o', '\0'};
-    char Crr[] = "Hello";
+///////////////////////////////////////////////////////////////
+//
+// Function Name : main
+// Output        : string values
+// Description   : Illustrates different ways of string creation in C.
+// Author        : Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+// Application : Clarifies how arrays and string literals differ in initialization.
+//
+///////////////////////////////////////////////////////////////
+
+int main() {
+    char Arr[6] = {'H', 'e', 'l', 'l', 'o', '\0'};   // fixed-size char array
+    char Brr[]  = {'H', 'e', 'l', 'l', 'o', '\0'};   // array with inferred size
+    char Crr[]  = "Hello";                           // string literal
 
     printf("%s\n", Arr);
     printf("%s\n", Brr);
     printf("%s\n", Crr);
+
+    return 0;
 }
