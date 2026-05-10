ae3e337949523e4dc4e74f209e35ee9f55e78464
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:43:31 +0530

    Update StringTraversal.c

--- c/StringTraversal.c
+++ c/StringTraversal.c
@@ -1,26 +1,41 @@
-#include<stdio.h>
+///////////////////////////////////////////////////////////////
+//
+// Function Name : StrLenX, main
+// Output        : char values, int length
+// Description   : Shows pointer traversal and manual string length calculation.
+// Author        : Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
 
-int StrLenX(char *ptr){ // Call By Address.
-    int i=0;
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+// Application : Explains how pointer increments traverse string and count length.
+//
+///////////////////////////////////////////////////////////////
+
+int StrLenX(char *ptr) { // Call By Address
+    int i = 0;
     while (*ptr != '\0') {
-       i++;
-       ptr++;
+        i++;
+        ptr++;
     }
     return i;
 }
-int main() {
 
+int main() {
     char str[] = "Ganesh";
     char* ptr = NULL;
 
-    ptr = str;                  //100
+    ptr = str;                  // points to first character
 
-    while (*ptr!='\0'){
-        printf("%c\n", *ptr);       
-        ptr++;                     
+    while (*ptr != '\0') {
+        printf("%c\n", *ptr);   // print each character
+        ptr++;                  // move pointer forward
     }
 
-    int result = StrLenX(str); //StrLenX(100);
+    int result = StrLenX(str);  // call function with base address
     printf("\n\n\nLength is: %d\n", result);
 
     return 0;    
