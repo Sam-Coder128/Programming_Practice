1563fe348f99330f3ab362045b10af4da30b0ee6
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:40:58 +0530

    Update SizeOfTypes.c

--- c/SizeOfTypes.c
+++ c/SizeOfTypes.c
@@ -1,4 +1,19 @@
+///////////////////////////////////////////////////////////////
+//
+// Function Name : main
+// Output        : sizes of short int, long int, unsigned int, signed int
+// Description   : Shows usage of sizeof operator on integer types.
+// Author        : Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
 #include <stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+// Application : Explains memory size differences across integer variations.
+//
+///////////////////////////////////////////////////////////////
 
 int main() {
     printf("Size of short int: %lu bytes\n", sizeof(short int));
