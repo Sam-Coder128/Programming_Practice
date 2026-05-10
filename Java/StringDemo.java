bc212a646adce9002ca6895438b9073bbdbf2424
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-10 14:23:36 +0530

    Update StringDemo.java

--- Java/StringDemo.java
+++ Java/StringDemo.java
@@ -1,8 +1,22 @@
-class StringDemo{
+///////////////////////////////////////////////////////////////
+//
+// Class Name : StringDemo
+// Variables : String S1, String S2
+// Description: Explains string object creation and length method usage.
+// Author:      Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+class StringDemo {
+    ///////////////////////////////////////////////////////////////
+    //
+    // Application : Highlights string printing and length evaluation.
+    //
+    ///////////////////////////////////////////////////////////////
     public static void main(String[] args) {
         
-        String S1 = "Hello";
-        String S2 = new String("Ganesh");
+        String S1 = "Hello";                  // string literal
+        String S2 = new String("Ganesh");     // new object
 
         System.out.println(S1);
         System.out.println(S1);
