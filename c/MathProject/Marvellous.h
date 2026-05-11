11bf506b36ed7f0ad2efc34bf134887cd50730cc
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-05-11 11:27:31 +0530

    Create Marvellous.h

---
+++ c/MathProject/Marvellous.h
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////
+//
+// Header File : Marvellous.h
+// Description : Illustrates prototypes of arithmetic functions.
+// Author      : Samruddh Shivkumar Birajdar
+//
+///////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int Addition(int, int);       
+int Substraction(int, int);   
+int Multiplication(int, int);
+int Division(int, int);
+int Modulus(int, int);
